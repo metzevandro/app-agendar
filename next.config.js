032ad27891d970c271160d893ba617514f619f5d
metzevandro <metzxevandro@gmail.com>
2024-04-30 08:37:21 -0300

    [BUILDING]

--- next.config.js
+++ next.config.js
@@ -1,9 +1,23 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-            config.externals = [...config.externals, 'bcrypt'];
-            return config;
-        },
+    webpack: (config, { isServer }) => {
+        // Exclude 'bcrypt' from server-side webpack bundling
+        if (isServer) {
+            config.externals.push('bcrypt');
+        }
+
+        // Add TypeScript loader for files in 'design-system-zeroz' package
+        config.module.rules.push({
+            test: /\.tsx?$/,
+            include: /design-system-zeroz/,
+            use: {
+                loader: 'ts-loader',
+                options: {
+                    allowTsInNodeModules: true,    },
+            },
+        });
+
+        return config;
+    },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
